83234bec372c5a8efc53e4f2ac0dc1b3d58e7822
kepler-byte <canthrwatrc@gmail.com>
2025-08-06 10:46:19 +0700

    feat: add date to greeting

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 import config
+import datetime
 
 def say_hello(name):
     now = datetime.datetime.now()
     print(f"Hello, {name}! from {config.APP_NAME}")
     print(f"Today is {now.strftime('%Y-%m-%d')}")
+
 
 if __name__ == "__main__":
     say_hello("hello")
